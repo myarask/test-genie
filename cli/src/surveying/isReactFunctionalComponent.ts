b730480f6f8ec873f9de44e7963a9421d2deb825
Mark Yaraskavitch <36746340+myarask@users.noreply.github.com>
2023-04-01 21:15:58 -0400

    try again

--- cli/src/surveying/isReactFunctionalComponent.ts
+++ cli/src/surveying/isReactFunctionalComponent.ts
@@ -5,21 +5,23 @@ export function isReactFunctionalComponent(node: ts.Node): boolean {
     return false;
   }
 
-  // Check if the node returns a JSX element
-  function isReturningJSXElement(node: ts.Node): boolean {
-    if (ts.isReturnStatement(node) && node.expression && ts.isJsxElement(node.expression)) {
-      return true;
-    }
-    return false;
+  // Check if the node is a JSX node
+  function isJSXNode(node: ts.Node): boolean {
+    return (
+      ts.isJsxElement(node) ||
+      ts.isJsxSelfClosingElement(node) ||
+      ts.isJsxExpression(node) ||
+      ts.isJsxFragment(node)
+    );
   }
 
-  // Traverse the AST to find a return statement with a JSX element
-  function traverse(node: ts.Node): boolean {
-    if (isReturningJSXElement(node)) {
+  // Traverse the AST to find any JSX nodes
+  function visit(node: ts.Node): boolean {
+    if (isJSXNode(node)) {
       return true;
     }
-    return ts.forEachChild(node, traverse) || false;
+    return ts.forEachChild(node, visit) || false;
   }
 
-  return traverse(node);
+  return visit(node);
 }
