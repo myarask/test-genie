b82131252f3414c41a7b95e77052c06258475aac
Mark Yaraskavitch <36746340+myarask@users.noreply.github.com>
2023-04-09 10:42:32 -0400

    drop in snippet

---
+++ cli/src/fdsf.ts
@@ -0,0 +1,40 @@
+import * as ts from "typescript";
+
+type ConditionNode = {
+  type: "condition";
+  condition: string;
+} | {
+  type: "and" | "or";
+  left: ConditionNode;
+  right: ConditionNode;
+} | {
+  type: "not";
+  operand: ConditionNode;
+};
+
+function extractConditions(expression: ts.Expression): ConditionNode {
+  if (ts.isBinaryExpression(expression)) {
+    if (expression.operatorToken.kind === ts.SyntaxKind.AmpersandAmpersandToken) {
+      return {
+        type: "and",
+        left: extractConditions(expression.left),
+        right: extractConditions(expression.right),
+      };
+    } else if (expression.operatorToken.kind === ts.SyntaxKind.BarBarToken) {
+      return {
+        type: "or",
+        left: extractConditions(expression.left),
+        right: extractConditions(expression.right),
+      };
+    }
+  } else if (ts.isPrefixUnaryExpression(expression) && expression.operator === ts.SyntaxKind.ExclamationToken) {
+    return {
+      type: "not",
+      operand: extractConditions(expression.operand),
+    };
+  }
+  return {
+    type: "condition",
+    condition: expression.getText(),
+  };
+}
